ca6d11856fa976cb0f1c7855ea309f5bf542bb73
Maksim Chuyashkou <maksimchuyashkou@gmail.com>
2022-03-13 22:37:42 +0300

    Hello Task 1

--- src/com/chuyashkou/HelloWorld.java
+++ src/com/chuyashkou/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Hello task1");
         System.out.println("Hello task2");
+        System.out.println("Hello task3 -> dev");
     }
 }
