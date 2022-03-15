88a46d5cc0a9c0b6ed1109d99fc9a67eaa53d068
Maksim Chuyashkou <maksimchuyashkou@gmail.com>
2022-03-15 21:55:38 +0300

    add task 2

---
+++ javaCoreTasks/src/chuyashkou/lesson3/Task2.java
@@ -0,0 +1,16 @@
+package chuyashkou.lesson3;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 10;
+        int c = 10;
+
+        if (a + b > c && a + c > b && b + c > a) {
+            System.out.println("Треугольник существует:)");
+        } else {
+            System.out.println("Треугольник не существует :(");
+        }
+    }
+}
