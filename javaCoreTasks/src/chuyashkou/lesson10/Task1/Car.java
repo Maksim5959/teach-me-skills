d7396b2eab2f060970b7b1da28014b923ce2e15a
Maksim Chuyashkou <maksimchuyashkou@gmail.com>
2022-04-21 16:12:59 +0300

    added exceptions task

---
+++ javaCoreTasks/src/chuyashkou/lesson10/Task1/Car.java
@@ -0,0 +1,61 @@
+package chuyashkou.lesson10.Task1;
+
+import java.util.Random;
+
+public class Car {
+
+    private String brand;
+    private int speed;
+    private double price;
+
+    public Car() {
+    }
+
+    public Car(String brand, int speed, double price) {
+        this.brand = brand;
+        this.speed = speed;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void start() throws CarException {
+        Random random = new Random();
+        if (random.nextInt(21) % 2 == 0) {
+            throw new CarException(toString() + " didn't start." , new ArithmeticException(), true, false);
+        } else {
+            System.out.println(this.toString() + " started.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "The car{" +
+                "brand='" + brand + '\'' +
+                ", speed=" + speed +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ javaCoreTasks/src/chuyashkou/lesson10/Task1/CarException.java
@@ -0,0 +1,23 @@
+package chuyashkou.lesson10.Task1;
+
+public class CarException extends Exception {
+
+    public CarException() {
+    }
+
+    public CarException(String message) {
+        super(message);
+    }
+
+    public CarException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CarException(Throwable cause) {
+        super(cause);
+    }
+
+    public CarException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
