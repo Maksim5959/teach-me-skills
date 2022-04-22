8d5ec533e91a37cf5932faab9292cbc076c28a7f
Maksim Chuyashkou <maksimchuyashkou@gmail.com>
2022-04-22 19:40:54 +0300

    added new sorting algorithms

--- javaCoreTasks/src/chuyashkou/sortingAlgorithms/QuickSort.java
+++ javaCoreTasks/src/chuyashkou/sortingAlgorithms/QuickSort.java
@@ -4,32 +4,32 @@ public class QuickSort {
 
     //24ms, 100000el
     //7192ms, 100000000el
-    public static int [] quickSort(int [] values,int lowIndex,int highIndex){
+    public static int[] quickSort(int[] values, int lowIndex, int highIndex) {
         int left = lowIndex;
         int right = highIndex;
-        int pivot = values [(left+right)/2];
+        int pivot = values[(left + right) / 2];
 
         do {
-            while (values [left] < pivot){
+            while (values[left] < pivot) {
                 left++;
             }
-            while (values [right] > pivot){
+            while (values[right] > pivot) {
                 right--;
             }
 
-            if (left <= right){
-                int tmp = values [left];
-                values [left] = values [right];
-                values [right] = tmp;
+            if (left <= right) {
+                int tmp = values[left];
+                values[left] = values[right];
+                values[right] = tmp;
                 left++;
                 right--;
             }
-        }while (left < right);
-        if (lowIndex < right){
-            quickSort(values,lowIndex,right);
+        } while (left < right);
+        if (lowIndex < right) {
+            quickSort(values, lowIndex, right);
         }
-        if (left < highIndex){
-            quickSort(values,left,highIndex);
+        if (left < highIndex) {
+            quickSort(values, left, highIndex);
         }
         return values;
     }
