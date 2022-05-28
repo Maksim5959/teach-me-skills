2ec9302e8edd49158979cfc1005c2592cd55844b
Maksim Chuyashkou <maksimchuyashkou@gmail.com>
2022-05-28 15:37:42 +0300

    new parsers tasks added

--- javaCoreTasks/src/main/java/chuyashkou/parsers/model/Book.java
+++ javaCoreTasks/src/main/java/chuyashkou/parsers/model/Book.java
@@ -1,24 +1,30 @@
 package chuyashkou.parsers.model;
 
+import chuyashkou.parsers.jaxb.LocalDateAdapter;
+import jakarta.xml.bind.annotation.*;
+import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import jakarta.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
-import
 import java.time.LocalDate;
 import java.util.List;
 
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
-@XmlRootElement
-@XmlType(name = "book")
+@Data
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = {"title","vendorCode","publishingDate","relatedBooks"})
 public class Book {
 
+    @XmlAttribute
     private String title;
+    @XmlAttribute
     private String vendorCode;
+    @XmlAttribute
+    @XmlJavaTypeAdapter(value = LocalDateAdapter.class, type = LocalDate.class)
     private LocalDate publishingDate;
+    @XmlElementWrapper(nillable = true, name = "relatedBooks")
+    @XmlElement(name = "relatedBook")
     private List<Book> relatedBooks;
 }
